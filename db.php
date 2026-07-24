8f115424367a33f2326794cb5a8d839cc3c9cf62
wangyibo8525-gif <wangyibo8525@gmail.com>
2026-07-24 20:51:16 +0700

    Add database connection script in db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+// db.php
+$host = 'localhost';
+$dbname = 'unity_studio';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database Connection Failed");
+}
+?>
